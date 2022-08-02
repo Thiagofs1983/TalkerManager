8dc351131531157e620321a77d3b3ce0ca3b2a1f
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-08-02 18:31:33 -0300

    feat: criar o endpoint get /talker/:id

--- index.js
+++ index.js
@@ -6,11 +6,22 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOK_STATUS = 404;
 const PORT = '3000';
 
 app.get('/talker', async (req, res) => {
   const talkers = await readFile(); 
   return res.status(HTTP_OK_STATUS).json(talkers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const talkers = await readFile();
+  const { id } = req.params;
+  const findTalker = talkers.find((talker) => Number(talker.id) === Number(id));
+  if (!findTalker) {
+    return res.status(HTTP_NOK_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+  } 
+  return res.status(HTTP_OK_STATUS).json(findTalker);
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
